0d9784dfef95dccdc6a3468b7776c00d666cb0b9
sree-chikati <srechi453.fusdk12@gmail.com>
2020-10-17 19:37:47 -0700

    done with intro page

--- scripts.js
+++ scripts.js
@@ -8,5 +8,5 @@ const char1 = document.querySelector("#char1")
 const tl = gsap.timeline();
 tl.from(logo, 1.2, {opcaity: 0, x: 30}, {opcaity: 1, x: 0},);
 tl.from(navList, 1.2, {opcaity: 0, x: 30}, {opcaity: 1, x: 0},);
-tl.from(intro, {opcaity:0, scale:0, duration: 1, ease:"back"})
+tl.from(intro, 1.2, {opcaity: 0, xPercent: 50}, {opcaity: 1, x: 0})
 tl.from(char1, {yPercent: 118, stagger: 0.1, duration: 0.8, ease:"back"})
